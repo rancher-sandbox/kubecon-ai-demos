2d548538d1b442b6be23d4c11aa3b0ea187506d3
Mark Abrams <mabrams@helpguest.com>
2022-10-14 15:46:29 -0400

    updated arduino sketches i2c-setup.ino no has an option to install the default configuration as 'rock' or a closed fist. robot-control.ino is now configured to operate a 'cycle' based on configuration which means the i2c client can control every move after startup.

--- arduino/robot-control/robot-control.ino
+++ arduino/robot-control/robot-control.ino
@@ -3,6 +3,7 @@
 
 #define DEBUG 1
 
+
 //Initialize arduino i2c for config data
 I2CSlaveMode agent = new I2CSlaveMode();
 //Initialize Adafruit servo driver board to address 0x40 (the default)
@@ -20,43 +21,15 @@ enum servo {
   thumb,
   wrist
 };
-
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
-// Tweak them by setting values in EEPROM registers
-// Min starts at about 600 microseconds 
-// Max starts at about 2400 microseconds 
-enum runtime_offset {
-  gesture_hold = 0x04,//Register with a multiplier for number of seconds to hold the gesture 
-  /*********************
-   * the activity_state register value can be used by the client to activate or deactivate the current gesture
-   * If the state is less than 0xFF, the program will run the number of times
-   * specified by the value.
-   * If the activity_state value is 0x00, the program will stop
-   * The program checks the state periodically 
-   * based on the timing interval specified using gesture_hold value
-   *********************************/
-  activity_state = 0x05 // Register indicating whether to DELAY, RUN, or STOP processing
-};
-
-// Will be set to the value of the activity_state register if the cur and stored state are different
-uint8_t active_state = 0x00;
-uint8_t cur_state = active_state;
-
-uint8_t const signature_len = 16;
-// Default is do nothing
-uint8_t sig_pinky[signature_len] = {0};
-uint8_t sig_ring[signature_len] = {0};
-uint8_t sig_middle[signature_len] = {0};
-uint8_t sig_pointer[signature_len] = {0};
-uint8_t sig_thumb[signature_len] = {0};
-uint8_t sig_wrist[signature_len] = {0};
-
-uint8_t hand[][signature_len] = { {sig_pinky}, {sig_ring}, {sig_middle}, {sig_pointer}, {sig_thumb}, {sig_wrist} };
-
-//Gesture registers (signatures)
+/********************
+ * WARNING: these two arrays are tightly coupled with the servo enum as the index to the arrays
+ * Use caution when modifying
+ * char digit[6][7]
+ * uint8_t signatures[6]
+ ********************/
+char digit[6][7] = { {"pinky"}, {"ring"}, {"middle"}, {"index"}, {"thumb"}, {"wrist"} };
+uint8_t signatures[6] = { 0x10, 0x20, 0x30, 0x40, 0x50, 0x60 };
+//Signature instruction set (signature offsets)
 /*************** 
  *  Each servo can be uniquely manipulated 
  *  Each servo has 16 registers for detailing its range and desired position
@@ -84,34 +57,61 @@ uint8_t hand[][signature_len] = { {sig_pinky}, {sig_ring}, {sig_middle}, {sig_po
  *  In the example above, the first 5 servos are enabled as indicated by the 16th register [F] 
  *    and the last servo is disabled
  */
-enum gesture_offset {
-  g_pinky   = 0x10,
-  g_ring    = 0x20,
-  g_middle  = 0x30,
-  g_pointer = 0x40,
-  g_thumb   = 0x50,
-  g_wrist   = 0x60,
-} ;
-
-enum signature_offset {
+enum signature_offsets {
   min_extend = 0x0,
   max_extend = 0x2,
   min_relax = 0x4,
   max_relax = 0x6,
-  sig_position = 0xA,
+  sig_position = 0x8,
+  sig_relax = 0xA,
   sig_delay = 0xB,
   sig_activate = 0xF,
 };
 /**************
- * End Gesture registers (signatures) data
+ * End signature offsets data
  *****************************/
 
+// Depending on your servo make, the pulse width min and max may vary, you 
+// want these to be as small/large as possible without hitting the hard stop
+// for max range. You'll have to tweak them as necessary to match the servos you
+// have!
+// Tweak them by setting values in EEPROM registers
+// Min starts at about 600 microseconds 
+// Max starts at about 2400 microseconds 
+enum runtime_offset {
+  gesture_hold = 0x04,//Register with a multiplier for number of seconds to hold the gesture 
+  /*********************
+   * the run_count register value can be used by the client to activate or deactivate the current gesture
+   * If the state is less than 0xFF, the program will run the number of times
+   * specified by the value.
+   * If the run_count value is 0x00, the program will stop
+   * The program checks the state periodically 
+   * based on the timing interval specified using gesture_hold value
+   *********************************/
+  run_count = 0x05 // Register indicating how many cycles to run the program
+};
+
+// Will be set to the value of the run_count register if the cur and stored state are different
+uint8_t active_state = 0x00;
+uint8_t cur_state = active_state;
+
+uint8_t const signature_len = 16;
+// Default is do nothing
+uint8_t sig_pinky[signature_len] = {0};
+uint8_t sig_ring[signature_len] = {0};
+uint8_t sig_middle[signature_len] = {0};
+uint8_t sig_pointer[signature_len] = {0};
+uint8_t sig_thumb[signature_len] = {0};
+uint8_t sig_wrist[signature_len] = {0};
+
+uint8_t hand[][signature_len] = { {sig_pinky}, {sig_ring}, {sig_middle}, {sig_pointer}, {sig_thumb}, {sig_wrist} };
 
 uint16_t get_double_reg(uint8_t reg) {
-  uint16_t value = agent.getRegister(reg);
-  value <<= 8;
-  value |= agent.getRegister(reg+1);
-  return value;
+  uint16_t upper = agent.getRegister(reg);
+  uint16_t lower = agent.getRegister(reg+1);
+  uint16_t final_value = upper << 8;
+  final_value = final_value | lower;
+  return final_value;
 }
 
 
@@ -121,7 +121,6 @@ void setup() {
   Serial.println("\nFinger Control");
   Serial.print("I2C address:");
   Serial.println(agent.getAddress(), HEX);
-
   pwm.begin(); //This calls Wire.begin()
   pwm.sleep();
   pwm.setPWMFreq(SERVO_FREQ);  // This is the maximum PWM frequency
@@ -135,7 +134,8 @@ void setup() {
 }
 
 void start_activity() {
-    uint8_t next_state = (uint8_t)agent.getRegister(activity_state);
+    
+    uint8_t next_state = (uint8_t)agent.getRegister(run_count);
     if (next_state != cur_state) {
       cur_state = next_state;
       active_state = next_state;
@@ -143,19 +143,15 @@ void start_activity() {
 }
 
 void load_gesture() {
+  
   for (uint8_t i = 0; i < signature_len; i++) {
-    hand[pinky][i] = agent.getRegister(g_pinky+i);
-    hand[ring][i] = agent.getRegister(g_ring+i);
-    hand[middle][i] = agent.getRegister(g_middle+i);
-    hand[pointer][i] = agent.getRegister(g_pointer+i);
-    hand[thumb][i] = agent.getRegister(g_thumb+i);
-    hand[wrist][i] = agent.getRegister(g_wrist+i);
+    hand[pinky][i] = agent.getRegister(signatures[pinky]+i);
+    hand[ring][i] = agent.getRegister(signatures[ring]+i);
+    hand[middle][i] = agent.getRegister(signatures[middle]+i);
+    hand[pointer][i] = agent.getRegister(signatures[pointer]+i);
+    hand[thumb][i] = agent.getRegister(signatures[thumb]+i);
+    hand[wrist][i] = agent.getRegister(signatures[wrist]+i);
   }
-  #ifdef DEBUG
-    char finger[20];
-    sprintf(finger, "Pinky: 0x%02X%02x", hand[pinky][sig_position],hand[pinky][sig_position+1]);
-    Serial.println(finger);
-  #endif
 }
 
 void gesture() {
@@ -168,38 +164,63 @@ void gesture() {
 }
 
 void relax(servo f) {
-  uint16_t relax_min = get_double_reg(hand[f][min_relax]);
-  uint16_t relax_max = get_double_reg(hand[f][max_relax]);
-  uint16_t position = get_double_reg(hand[f][sig_position]);
-  pwm.writeMicroseconds((byte)f, constrain(position, relax_min, relax_max));
+  uint16_t min = get_double_reg(signatures[f]+min_relax);
+  uint16_t max = get_double_reg(signatures[f]+max_relax);
+  uint16_t pos = get_double_reg(signatures[f]+sig_position);
+  pwm.writeMicroseconds((byte)f, constrain(pos, min, max));
 }
 
 void set_position(servo f) {
-  uint16_t extend_min = get_double_reg(hand[f][min_extend]);
-  uint16_t extend_max = get_double_reg(hand[f][max_extend]);
-  uint16_t position = get_double_reg(hand[f][sig_position]);
-  pwm.writeMicroseconds((byte)f, constrain(position, extend_min, extend_max));
-  if (hand[f][0xA] > 0) {
-    delay(hand[f][sig_delay]);
-    relax(f);
+  
+  if ( hand[f][sig_activate] > 0 ) {
+    uint16_t min = get_double_reg(signatures[f]+min_extend);
+    uint16_t max = get_double_reg(signatures[f]+max_extend);
+    uint16_t pos = get_double_reg(signatures[f]+sig_position);
+    pwm.writeMicroseconds((byte)f, constrain(pos, min, max));
+    if (hand[f][sig_relax] > 0) {
+      delay(hand[f][sig_delay]);
+      relax(f);
+    }
   }
 }
 
 
 void loop() {
-
   unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    load_gesture();
+    load_gesture();    
 
     //If anything is not null do gesture()
-    if (active_state > 0){
+    if (active_state > 0){      
       pwm.wakeup();
-      gesture();
+      gesture();      
       active_state -= 1;
     }
   }
   pwm.sleep();
 }
+
+void _dump_digit(servo f) {
+  #ifdef DEBUG
+    
+    char buf[strlen(digit[f]) + 3];
+    sprintf(buf, "%s: ", digit[f]);
+    Serial.println();
+    Serial.print(buf);
+    _dump_instruction_set(f);
+    Serial.flush();
+  #endif
+}
+
+void _dump_instruction_set(servo f) {
+  char finger[50] = {0};
+  for( uint8_t i = 0; i < 16; i++ ) {
+    sprintf(finger + (i*3), "%02X ", hand[f][i]);
+  }
+  Serial.println(finger);
+}
+
+
+#undef DEBUG
